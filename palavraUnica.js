b2a2c51baeab91318ad50e7ef75f3998748b5b63
Carlos Guilherme <103211459+Guilherme-Dev-Java@users.noreply.github.com>
2025-06-22 19:00:57 -0300

    criando o arquivo palavrasUnica.js
    Ele cria um array com as palavras que não se repete na frase, assim retornando um array com palavras unicas.

---
+++ palavraUnica.js
@@ -0,0 +1,47 @@
+function palavrasUnicas(frase) {
+    // Remove pontuação e converte para minúsculas
+    frase = frase.replace(/[.,!?;:]/g, "").toLowerCase();
+
+    const palavras = frase.split(" ");
+    const unicas = [];
+
+    for (let i = 0; i < palavras.length; i++) {
+        let contador = 0;
+
+        for (let j = 0; j < palavras.length; j++) {
+            if (palavras[i] === palavras[j]) {
+                contador++;
+            }
+        }
+
+        if (contador === 1) {
+            let jaExiste = false;
+            for (let k = 0; k < unicas.length; k++) {
+                if (unicas[k] === palavras[i]) {
+                    jaExiste = true;
+                    break;
+                }
+            }
+
+            if (!jaExiste) {
+                unicas.push(palavras[i]);
+            }
+        }
+    }
+
+    return unicas;
+}
+
+// Exemplo com vírgula
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Digite uma frase: ", function(entrada) {
+    const resultado = palavrasUnicas(entrada);
+    console.log("Palavras únicas:", resultado);
+    rl.close();
+});
